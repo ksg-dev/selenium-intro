6713f749b75e33be38b60d3bd9a726a05763fd76
ksg-dev <ksg.dev.data@gmail.com>
2024-07-02 15:08:26 -0500

    finding elements using xpath

--- main.py
+++ main.py
@@ -17,10 +17,18 @@ driver.get("https://www.python.org")
 # Find search bar on python site
 search_bar = driver.find_element(By.NAME, value="q")
 # add . to access attributes
-print(search_bar.get_attribute("placeholder"))
+# print(search_bar.get_attribute("placeholder"))
 # Then we want to access submit button
 button = driver.find_element(By.ID, value="submit")
-print(button.size)
+# print(button.size)
+# Get an element without a name, class, or id
+# This is a link within an anchor tag within a div element w this name
+doc_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# print(doc_link.text)
+
+# Using XPATH - copy XPATH when you right click
+submit_bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+print(submit_bug_link.text)
 
 # # Close particular tab
 # driver.close()
